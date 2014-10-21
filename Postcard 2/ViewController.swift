8ee85d6476d078ae4f5138e0490b4263134d5be9
Tom Streep <Tom@Toms-iMac.local>
2014-10-20 22:07:42 -0400

    HEY

--- Postcard 2/ViewController.swift
+++ Postcard 2/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterName: UITextField!
+    @IBOutlet weak var enterMessage: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func sendPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessage.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterName.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterMessage.resignFirstResponder()
+        enterMessage.text = ""
+        
+        enterName.text = ""
+        
+        sendButton.setTitle(":)", forState: UIControlState.Normal)
+    }
 
 }
 
